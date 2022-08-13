9c2fdb179e3a18bb079644b253f786e663f740f3
Erikson <chris.s.erickson@gmail.com>
2022-08-13 12:16:13 -0400

    added port const

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // require express.js after npm init -y to create repo
 const express = require ('express');
 
+const PORT = process.env.PORT || 3001;
+
 // instantiate the server
 const app=express();
 
@@ -55,10 +57,9 @@ app.get('/api/animals', (req, res) => {
   });
 
 // method to make server listen
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-});
-
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
 // route for front end request
 const { animals } = require('./data/animals');
 
