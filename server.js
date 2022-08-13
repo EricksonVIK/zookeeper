41973b49df92670f1a0dac392a8623e1c29c653d
Erikson <chris.s.erickson@gmail.com>
2022-08-13 14:14:28 -0400

    added the ability to add validated data

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+// adding ability to rewrite the animal.json file
+const fs = require('fs');
+const path = require('path');
+
+
 // require express.js after npm init -y to create repo
 const express = require ('express');
 
@@ -5,6 +10,11 @@ const PORT = process.env.PORT || 3001;
 
 // instantiate the server
 const app=express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 // filter results function
 function filterByQuery(query, animalsArray) {
@@ -52,6 +62,38 @@ const result = animalsArray.filter(animal => animal.id === id)[0];
 return result;
 }  
 
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    // creates variable and pushes to main animal array
+    const animal = body;
+    animalsArray.push(animal);
+    // rewrites data file
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+      );
+  
+    // return finished code to post route for response
+    return body;
+  }
+
+// validation funtion
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
 // add route type
 app.get('/api/animals', (req, res) => {
     // .send command -// res.send('Hello!');
@@ -72,7 +114,22 @@ if (result) {
     res.send(404);
 }
 });
-// method to make server listen
+
+// receives and tranfers to animal array
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+  });
+
+  // method to make server listen
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
